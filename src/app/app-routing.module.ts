2087ab0af649ecc6121f293c6e1d11fa974c812b
Alejandra Ramirez-Loyer <alejandra.ramirez-loyer@fms-ea.com>
2024-10-01 14:02:02 +0200

    creation navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
+  {
+  path: 'navbar', component : NavbarComponent
+  },
   {
   path : 'card', component : CardComponent
   }
